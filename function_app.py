1583bfb688ccce5de9c52579953d18866ad7ab07
Felipe Whitaker <felipe.whitaker@statkraft.com>
2025-02-11 18:06:33 -0300

    [feat] Add `temp_triggered`

--- function_app.py
+++ function_app.py
@@ -3,9 +3,19 @@ import logging
 
 app = func.FunctionApp()
 
-@app.blob_trigger(arg_name="myblob", path="raw-data/temp",
+@app.blob_trigger(arg_name="myblob", path="sample-workitems",
                                connection="4931ba_STORAGE") 
 def blob_triggered(myblob: func.InputStream):
+    # is triggered both for new blobs in `sample-workitems` and
+    #   `sample-workitems/temp`
+    logging.info(f"Python blob trigger function processed blob"
+                f"Name: {myblob.name}"
+                f"Blob Size: {myblob.length} bytes")
+
+@app.blob_trigger(arg_name="myblob", path="sample-workitems/temp",
+                               connection="4931ba_STORAGE") 
+def temp_triggered(myblob: func.InputStream):
+    # is not triggered when a blob enters `sample-workitems/temp`
     logging.info(f"Python blob trigger function processed blob"
                 f"Name: {myblob.name}"
                 f"Blob Size: {myblob.length} bytes")
